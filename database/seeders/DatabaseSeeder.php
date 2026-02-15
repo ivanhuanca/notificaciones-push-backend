37fab3786b26d9a519d7b38123690cbff22fa912
Ivan Huanca <ivandhp@gmail.com>
2026-02-14 23:17:40 -0500

    database/seeders/DatabaseSeeder.php crear 10 usuarios aleatorios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create([
+            'password' => bcrypt('~@L2@b~~apkrJbJ@buuRx.B'),
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
